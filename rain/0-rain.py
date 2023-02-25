9a756754c4742af57a39e023dfe55933ca03da15
floriancheick <f.nganji@alustudent.com>
2023-02-25 18:32:44 +0000

    0-rain.py

---
+++ rain/0-rain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Module 0-rain"""
+
+def rain(walls):
+	water_temp = 0
+	water = 0
+	size = len(walls)
+	doom = 0
+
+	if size <= 0:
+		return water_temp
+	for i in range(size):
+		if walls[i] >= walls[doom]:
+			doom = i
+			water_temp = 0
+		else:
+			water +=walls[doom] - walls[i]
+			water_temp += walls[doom] - walls[i]
+
+	if doom < size -1:
+		water -= water_temp
+		# last peak wall
+		peak_wall = doom
+		doom = size - 1
+
+		for i in range(size - 1, peak_wall, -1):
+			if walls[i] >= walls[doom]:
+				doom = i
+			else:
+				water += walls[doom] - walls[i]
+	return water
